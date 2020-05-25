b4485206140797b7f8eb5689f767d56f16a5c191
Jonáš Rosecký <rosecky.jonas@centrum.cz>
2020-05-25 18:05:59 +0200

    Heroku listen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 const login = require("facebook-chat-api");
 const GIFEncoder = require('gifencoder');
 var Canvas = require('canvas')
